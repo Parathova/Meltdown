3332ea74556cb5ba546bdd45e46bfa90090172c1
MicrophoneAle <microphoonale@gmail.com>
2023-08-25 14:55:41 -0400

    w

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import sys
 from cx_Freeze import setup, Executable
 
 build_exe_options = {
-    "excludes": ["tkinter", "unittest"],
+    "excludes": ["unittest"],
     "zip_include_packages": ["encodings", "PySide6"],
 }
 
 base = "Win32GUI" if sys.platform == "win32" else None
 
 setup(
-    name="guifoo",
+    name="Meltdown",
     version="0.1",
-    description="My GUI application!",
+    description="Woo climate change",
     options={"build_exe": build_exe_options},
-    executables=[Executable("guifoo.py", base=base)],
+    executables=[Executable("main.py", base=base)],
 )
