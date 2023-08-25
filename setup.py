120752e02d5f8418b690eee2452b2acdc8588c06
MicrophoneAle <microphoonale@gmail.com>
2023-08-25 15:18:10 -0400

    w

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from cx_Freeze import setup, Executable
 build_exe_options = {
     "excludes": ["unittest"],
     "zip_include_packages": ["encodings", "PySide6"],
+    'include_files':['tcl86t.dll', 'tk86t.dll', 'background_img.png', 'icon.png', 'news.png', 'pol_bar,png',
+                     'tab1.png', 'tab2.png', 'tab3.png', 'tab4.png', 'water_1.png', 'water_p_1.png', 'world.png',
+                     'buy.png', 'exp_con.png', 'exp_glo.png', 'exp_nat.png', 'exp_prov.png', 'lock.png']
 }
 
 base = "Win32GUI" if sys.platform == "win32" else None
