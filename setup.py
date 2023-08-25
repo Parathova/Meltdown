e5ba40c638fc1655fea91577f921492cd01f7d68
MicrophoneAle <microphoonale@gmail.com>
2023-08-25 14:39:54 -0400

    w

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ base = None
 if sys.platform == 'win32':
     base = "Win32GUI"
 
-executables = [cx_Freeze.Executable("main.py", base = base, targetName = "meltdown")]
+executables = [cx_Freeze.Executable("main.py", base = base, targetName = "NAME_OF_EXE")]
 
 cx_Freeze.setup(
     name = "meltdown",
