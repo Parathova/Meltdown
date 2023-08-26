14bece387b9367252a685df06522cd338d7ef8c3
MicrophoneAle <microphoonale@gmail.com>
2023-08-25 21:29:58 -0400

    w

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ build_exe_options = {
     "zip_include_packages": ["encodings", "PySide6"],
     'include_files':['tcl86t.dll', 'tk86t.dll', 'background_img.png', 'icon.png', 'news.png', 'pol_bar,png',
                      'tab1.png', 'tab2.png', 'tab3.png', 'tab4.png', 'water_1.png', 'water_p_1.png', 'world.png',
-                     'buy.png', 'exp_con.png', 'exp_glo.png', 'exp_nat.png', 'exp_prov.png', 'lock.png']
+                     'buy.png',  'lock.png',
+                     'exp_con_des.png', 'exp_con.png', 'exp_glo_des.png', 'exp_glo.png', 'exp_nat_des.png', 'exp_net.png',
+                     'exp_prov_des.png', 'exp_prov.png', 'pro_1_des.png', 'pro_1.png', 'pro_2_des.png', 'pro_2.png',
+                     'pro_3_des.png', 'pro_3.png', 'pro_4_des.png', 'pro_4.png', 'background.png'
+                    ]
 }
 
 base = "Win32GUI" if sys.platform == "win32" else None
